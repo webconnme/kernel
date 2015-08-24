5bdeba7da042d4d34fc02d6c8943925fc2e80a4b
byungbok <erunsee@falinux.com>
2015-08-24 10:58:38 +0900

    초기 커밋

---
+++ include/generated/compile.h
@@ -0,0 +1,6 @@
+/* This file is auto generated, version 21 */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#21 Mon Aug 24 10:28:26 KST 2015"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "lbb-ubuntu"
+#define LINUX_COMPILER "gcc version 4.6.3 20120201 (prerelease) (1.14.1 - nexell) "
---
+++ include/generated/utsrelease.h
@@ -0,0 +1,1 @@
+#define UTS_RELEASE "3.10.84-g6e001c2-dirty"
