b31b7aa15f43a63faeae4956d9ecb2261773f68a
byungbok <erunsee@falinux.com>
2015-09-08 16:01:34 +0900

    wifi driver add wq

--- menuconfig_nxp2120.sh
+++ menuconfig_nxp2120.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-make ARCH=arm  CROSS_COMPILE=/opt/crosstools/arm-eabi-4.6.3-glibc-2.13/bin/arm-generic-linux-gnueabi- menuconfig 
+make ARCH=arm  CROSS_COMPILE=arm-generic-linux-gnueabi- menuconfig 
 
