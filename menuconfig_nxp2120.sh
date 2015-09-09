0aa21360baaa0a698a4209c20bb5921792d5a41a
byungbok <erunsee@falinux.com>
2015-09-09 17:38:28 +0900

    webconn_defconfig에 crosscompile설정 커널 버전 local로 변경 현재 버전 .001로 설정 menuconfig스크립트 수정

--- menuconfig_nxp2120.sh
+++ menuconfig_nxp2120.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-make ARCH=arm  CROSS_COMPILE=arm-generic-linux-gnueabi- menuconfig 
+#make ARCH=arm  CROSS_COMPILE=arm-generic-linux-gnueabi- menuconfig 
+make ARCH=arm  CROSS_COMPILE=arm-generic-linux-gnueabi- menuconfig O=../build/
+
 
