ae2c3a79339f43f248f17783c27cf12cdafa6732
병복 <erunsee@falinux.com>
2015-09-30 12:22:25 +0900

    update

--- menuconfig_nxp2120.sh
+++ menuconfig_nxp2120.sh
@@ -1,9 +1,3 @@
 #!/bin/sh
 #make ARCH=arm  CROSS_COMPILE=arm-generic-linux-gnueabi- menuconfig 
-<<<<<<< HEAD
 make ARCH=arm  CROSS_COMPILE=arm-generic-linux-gnueabi- menuconfig O=../build/
-=======
-make ARCH=arm  CROSS_COMPILE=arm-generic-linux-gnueabi- menuconfig O=/work/build/
->>>>>>> 6c7bda510d088a72e5aa8c465887c55af1f70bfb
-
-
