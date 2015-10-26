64083a6e256e41437e5810713137491bad0a6620
David You <frog@falinux.com>
2015-10-26 18:43:54 +0900

    add zeroboot system call

--- arch/arm/kernel/sys_arm.c
+++ arch/arm/kernel/sys_arm.c
@@ -37,3 +37,34 @@ asmlinkage long sys_arm_fadvise64_64(int fd, int advice,
 {
 	return sys_fadvise64_64(fd, offset, len, advice);
 }
+
+#define    ZEROBOOT_NONE          0   // 아무것도 하지 않는다. 내부적으로 시험용으로 사용한다. 
+#define    ZEROBOOT_SNAPSHOT      1   // 스냅샷을 시작한다. 
+#define    ZEROBOOT_GET_STATE     2   // 제로부트 상태를 얻는다. 
+
+asmlinkage void * sys_zeroboot_syscall( int cmd, __user void *data )
+{
+	long  str_size;
+	char *kstr;
+	
+	printk( "SYSTEM CALL ZEROBOOT cmd = %d\n", cmd );
+	switch( cmd ){
+	case ZEROBOOT_NONE      : printk( "    ZEROBOOT_NONE\n"  ); 
+               	
+                               str_size = strlen_user( (char *) data );	
+							   kstr = kmalloc( str_size, GFP_KERNEL );
+    	                       copy_from_user ( kstr, (char *) data, str_size);
+		                       printk( "data = [%s]\n", kstr );
+		                       kfree( kstr );
+	
+	                           return (void *) str_size;
+							   
+    case ZEROBOOT_SNAPSHOT  : printk( "    ZEROBOOT_SNAPSHOT\n"  ); break;
+    case ZEROBOOT_GET_STATE : printk( "    ZEROBOOT_GET_STATE\n" ); break;
+	default                 : printk( "    UNKNOW CMD\n" );         break;
+	    
+	}
+	
+	return (void *) 0;
+}
+
